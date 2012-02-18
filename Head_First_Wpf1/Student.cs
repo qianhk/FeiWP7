4bdd5bf07c6776a3d1ab8db784d8458de93c2c1b
qhkyzf <qhkyzf@tom.com>
2012-02-18 10:10:05 +0800

    add a file

---
+++ Head_First_Wpf1/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace WpfApplication1
+{
+	class Student : INotifyPropertyChanged
+	{
+		string name;
+		int _id;
+		int _age;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public int Age
+		{
+			get
+			{
+				return _age;
+			}
+			set
+			{
+				_age = value;
+				if (PropertyChanged != null)
+				{
+					PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Age"));
+				}
+			}
+		}
+
+		public int Id
+		{
+			get
+			{
+				return _id;
+			}
+			set
+			{
+				_id = value;
+				if (PropertyChanged != null)
+				{
+					PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Id"));
+				}
+			}
+		}
+
+		public string Name
+		{
+			get
+			{
+				return name;
+			}
+			set
+			{
+				name = value;
+				if (PropertyChanged != null)
+				{
+					PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Name"));
+				}
+			}
+		}
+	}
+}
