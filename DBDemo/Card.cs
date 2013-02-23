d8cbd6adb1ee21741e4de1ccd74b8bccaaf643e9
kaiw8 <kai@win8>
2013-02-23 23:27:13 +0800

    can display data.

---
+++ DBDemo/Card.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+
+namespace DBDemo
+{
+	[Table]
+	public class Card
+	{
+		[Column (IsPrimaryKey=true)]
+		public int ID { get; set; }
+
+		[Column]
+		public string Name { get; set; }
+
+		[Column]
+		public string Email { get; set; }
+
+		[Column]
+		public string Address { get; set; }
+
+	}
+
+	public class SourceData : DataContext
+	{
+		public Table<Card> Cards;
+		public SourceData(string connectString) : base(connectString)
+		{
+			
+		}
+	}
+}
--- DBDemo/MainPage.xaml.cs
+++ DBDemo/MainPage.xaml.cs
@@ -15,10 +15,57 @@ namespace DBDemo
 {
 	public partial class MainPage : PhoneApplicationPage
 	{
+		private string dbPath = "isostore:/mydb.sdf";
 		// Constructor
 		public MainPage()
 		{
 			InitializeComponent();
+
+			//SourceData sd = new SourceData("isostore:/ttdb.sdf");
+			//if (!sd.DatabaseExists())
+			//{
+			//	sd.CreateDatabase();
+			//}
+		}
+
+		private void btnAdd_Click_1(object sender, RoutedEventArgs e)
+		{
+			SourceData sd = new SourceData(dbPath);
+			if (!sd.DatabaseExists())
+			{
+				sd.CreateDatabase();
+			}
+			try
+			{
+				Card card = new Card() { ID = 1, Name = "kai1", Email = "kai1@cc.com", Address = "地球" };
+				Card card2 = new Card() { ID = 2, Name = "测试中文", Email = "kai2@cc.com", Address = "月亮" };
+				sd.Cards.InsertOnSubmit(card);
+				sd.Cards.InsertOnSubmit(card2);
+				sd.SubmitChanges();
+			}
+			catch
+			{
+				MessageBox.Show("Data already exists");
+			}
+		}
+		
+		private void btnDel_Click_1(object sender, RoutedEventArgs e)
+		{
+			CardListBox.ItemsSource = null;
+			SourceData sd = new SourceData(dbPath);
+			var results = from item in sd.Cards select item;
+			foreach (var i in results)
+			{
+				sd.Cards.DeleteOnSubmit(i);
+			}
+			sd.SubmitChanges();
+		}
+
+		private void btnDis_Click_1(object sender, RoutedEventArgs e)
+		{
+			SourceData sd = new SourceData(dbPath);
+			var results = from item in sd.Cards select item;
+			CardListBox.ItemsSource = results.ToList();
 		}
 	}
 }
